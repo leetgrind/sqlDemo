8b9f75966f902e84e32e43e4bfe44118fd1351c6
leetgrind <leetgrind.org@gmail.com>
2023-11-13 18:15:14 -0500

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.xerial:sqlite-jdbc:3.43.2.2")
+    implementation("com.github.javafaker:javafaker:1.0.2")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import com.github.javafaker.Faker;
+
+import java.sql.*;
+import java.util.UUID;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection connection =
+                    DriverManager.getConnection("jdbc:sqlite:test.db");
+
+            Statement statement = connection.createStatement();
+
+            seedData(statement, 10);
+
+        }
+        catch (SQLException ex) {
+            System.out.println("Exception " + ex.getMessage());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    static void createTable(Statement statement) throws SQLException {
+        String createTableQuery = "CREATE TABLE AUTHOR (ID VARCHAR(100), NAME VARCHAR(20), BOOK VARCHAR(20));";
+        int result = statement.executeUpdate(createTableQuery);
+        statement.close();
+    }
+
+    static void seedData(Statement statement, int count) throws SQLException {
+
+        Faker faker = new Faker();
+
+        for(int i=0; i<count; i++) {
+
+            String id = UUID.randomUUID().toString();
+            String name = faker.name().name();
+            String bookName = faker.book().title();
+            // INSERT INTO AUTHOR VALUES('abc-abc-abc', 'name', 'bookName');
+            String insertQuery = "INSERT INTO AUTHOR VALUES('" + id + "', '" + name + "', '" + bookName + "');";
+            int result = statement.executeUpdate(insertQuery);
+        }
+
+
+        statement.close();
+
+    }
+}
